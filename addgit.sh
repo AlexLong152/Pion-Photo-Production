4e5b4fc8e588e1e02a9bc730f43aba4dd0b0312e
AlexLong152 <alexlong152@gmail.com>
2023-08-22 12:52:13 -0400

    added addgit.sh

---
+++ addgit.sh
@@ -0,0 +1,17 @@
+cp ../get_file.py .
+git add get_file.py
+git add thresholdOnebody/*
+git add thresholdTwobody/*
+
+git add common-densities/*
+
+git add common-densities/density-modules/*
+git add README.md
+git commit 
+
+rm get_file.py
+
+echo "To push to github run: git push -u origin main"
+echo "or: git push -uf origin main"
+echo "and log in with credentials"
+
